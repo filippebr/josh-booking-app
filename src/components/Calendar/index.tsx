fa6e3cf1eb401c59cf2c8a77c1fbfa8222a638d1
Filippe Braga <filippeffx@hotmail.com>
2023-06-06 17:42:52 -0300

    initial calendar

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,9 +1,10 @@
 import { type FunctionComponent } from "react"
+import ReactCalendar from 'react-calendar'
 
 interface indexProps {}
  
 const index: FunctionComponent<indexProps> = ({}) => {
-  return <div>index</div>
+  return <div><ReactCalendar minDate={new Date()} className='REACT-CALENDAR p-2' view='month' onClickDay={(date) => console.log(date)} /></div>
 }
  
 export default index;
