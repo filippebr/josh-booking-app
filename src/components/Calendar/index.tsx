e06a29754e17939e76f57599000e4bbf6a93103d
Filippe Braga <filippeffx@hotmail.com>
2023-06-06 17:36:38 -0300

    calendar install

---
+++ src/components/Calendar/index.tsx
@@ -0,0 +1,9 @@
+import { type FunctionComponent } from "react"
+
+interface indexProps {}
+ 
+const index: FunctionComponent<indexProps> = ({}) => {
+  return <div>index</div>
+}
+ 
+export default index;
