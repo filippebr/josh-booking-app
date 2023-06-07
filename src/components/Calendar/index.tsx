965e630030b5eb3ef81da6105fedb176a1b72444
Filippe Braga <filippeffx@hotmail.com>
2023-06-06 21:40:19 -0300

    time interval iteration frontend

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,3 +1,4 @@
+import { add } from 'date-fns'
 import { useState } from 'react'
 import ReactCalendar from 'react-calendar'
 
@@ -16,12 +17,29 @@ export function Calendar() {
     if (!date.justDate) return
 
     const { justDate } = date
+
+    const beginning = add(justDate, { hours: 9 })
+    const end = add(justDate, { hours: 17 })
+    const interval = 30 // in minutes
+
+    const times = []
+    for (let i = beginning; i <= end; i = add(i, { minutes: interval })) {
+      times.push(i)
+    }
+
+    return times
   }
+
+  const times = getTimes()
 
   return (
     <div className="flex h-screen flex-col items-center justify-center ">
       {date.justDate ? (
-        <div className="flex gap-4 "></div>
+        <div className="flex gap-4 ">
+          {times?.map((time, i) => (
+            <div key={`time-${i}`}></div>
+          ))}
+        </div>
       ) : (
         <ReactCalendar
           minDate={new Date()}
