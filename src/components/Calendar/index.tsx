e49aceff94e45e3f8133e52d1698bc208a2c741a
Filippe Braga <filippeffx@hotmail.com>
2023-06-06 21:31:11 -0300

    installed date-fns

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -12,16 +12,24 @@ export function Calendar() {
     dateTime: null,
   })
 
+  const getTimes = () => {
+    if (!date.justDate) return
+
+    const { justDate } = date
+  }
+
   return (
     <div className="flex h-screen flex-col items-center justify-center ">
       {date.justDate ? (
-        <div></div>
+        <div className="flex gap-4 "></div>
       ) : (
         <ReactCalendar
           minDate={new Date()}
           className="REACT-CALENDAR p-2"
           view="month"
-          onClickDay={(date) => console.log(date)}
+          onClickDay={(date) =>
+            setDate((prev) => ({ ...prev, justDate: date }))
+          }
         />
       )}
     </div>
