92378a0899e2b8c0ae2f349521a35df04826f8b7
Filippe Braga <filippeffx@hotmail.com>
2023-06-07 23:33:44 -0300

    login page frontend

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,4 @@
 import { trpc } from '@/utils/trpc'
-import Image from 'next/image'
 import { ChangeEvent, useState } from 'react'
 import { HiLockClosed } from 'react-icons/hi'
 
@@ -23,7 +22,7 @@ export function Login() {
     <div className="flex min-h-full items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
       <div className="w-full max-w-md space-y-8">
         <div>
-          <Image
+          <img
             className="mx-auto h-12 w-auto"
             src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
             alt="Workflow"
@@ -41,7 +40,8 @@ export function Login() {
             </a>
           </p>
         </div>
-        <form className="mt-8 space-y-6" action="">
+
+        <form className="mt-8 space-y-6">
           <input type="hidden" name="remember" defaultValue="true" />
           <div className="-space-y-px rounded-md shadow-sm">
             <p className="pb-1 text-sm text-red-600">
@@ -59,14 +59,31 @@ export function Login() {
                 onChange={handleChange}
                 autoComplete="email"
                 required
-                className="relative block w-full appearance-none rounded-none rounded-t-md border"
+                className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                 placeholder="Email address"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                value={input.password}
+                onChange={handleChange}
+                autoComplete="current-password"
+                required
+                className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                placeholder="Password"
               />
             </div>
           </div>
 
           <div className="flex items-center justify-between">
-            <div>
+            <div className="flex items-center">
               <input
                 id="remember-me"
                 name="remember-me"
@@ -98,7 +115,7 @@ export function Login() {
                 e.preventDefault()
                 login(input)
               }}
-              className="group relative flex w-full justify-center rounded-md border border-transparent"
+              className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
             >
               <span className="absolute inset-y-0 left-0 flex items-center pl-3">
                 <HiLockClosed
@@ -111,7 +128,6 @@ export function Login() {
           </div>
         </form>
       </div>
-      login
     </div>
   )
 }
