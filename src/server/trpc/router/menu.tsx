35d34587b91f5753a038fad0d6b0cf61d51b7865
Filippe Braga <filippeffx@hotmail.com>
2023-06-14 16:55:02 -0300

    getMenuItems menu router backend

--- src/server/trpc/router/menu.tsx
+++ src/server/trpc/router/menu.tsx
@@ -1,3 +1,21 @@
-import { router } from '@/server/trpc'
+import { s3 } from '@/lib/s3'
+import { publicProcedure, router } from '@/server/trpc'
 
-export const menuRouter = router({})
+export const menuRouter = router({
+  getMenuItems: publicProcedure.query(async ({ ctx }) => {
+    const menuItems = await ctx.prisma.menuItem.findMany()
+
+    // Each menu item only contains its AWS key. Extend all items with their actually img url
+    const withUrls = await Promise.all(
+      menuItems.map(async (menuItem) => ({
+        ...menuItem,
+        url: await s3.getSignedUrlPromise('getObject', {
+          Bucket: 'youtube-implementation',
+          Key: menuItem.imageKey,
+        }),
+      })),
+    )
+
+    return withUrls
+  }),
+})
