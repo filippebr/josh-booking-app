933d0034355a4a9bfc7222ba967a0da4dbc88791
Filippe Braga <filippeffx@hotmail.com>
2023-06-14 21:29:12 -0300

    menu router cloudinary

--- src/server/trpc/router/menu.tsx
+++ src/server/trpc/router/menu.tsx
@@ -1,5 +1,5 @@
-import { s3 } from '@/lib/s3'
 import { publicProcedure, router } from '@/server/trpc'
+import cloudinary from 'cloudinary'
 
 // sleep
 export const sleep = (ms: number) =>
@@ -9,24 +9,23 @@ export const menuRouter = router({
   getMenuItems: publicProcedure.query(async ({ ctx }) => {
     const menuItems = await ctx.prisma.menuItem.findMany()
 
-    // Each menu item only contains its AWS key. Extend all items with their actually img url
+    // Each menu item only contains its Cloudinary public_id. Extend all items with their actual image URLs
     const withUrls = await Promise.all(
-      menuItems.map(async (menuItem) => ({
-        ...menuItem,
-        url: await s3.getSignedUrlPromise('getObject', {
-          Bucket: 'youtube-implementation',
-          Key: menuItem.imageKey,
-        }),
-      })),
+      menuItems.map(async (menuItem) => {
+        const url = cloudinary.v2.url(menuItem.id, {
+          width: 500,
+          height: 500,
+          crop: 'limit',
+          resource_type: 'auto',
+        })
+
+        return {
+          ...menuItem,
+          url,
+        }
+      }),
     )
 
     return withUrls
-  }),
-
-  checkMenuStatus: publicProcedure.mutation(async () => {
-    // Handle menu checking logic
-    await sleep(1000)
-
-    return { success: true }
   }),
 })
